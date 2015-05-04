73fd78b59a49aa9543808ea094cf4106821db678
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-05-04 10:47:30 +0200

    add file-locking

--- daemon11.py
+++ daemon11.py
@@ -57,10 +57,20 @@ def do_work():
 def do_report(result):
 	# Get the time and date in human-readable form and UN*X-epoch...
 	outDate = commands.getoutput("date '+%F %H:%M:%S, %s'")
+	flock='/tmp/raspdiagd-11.lock'
+	lock(flock)
 	f = file('/tmp/11-t-cpu.csv', 'a')
 	f.write('{0}, {1}\n'.format(outDate, float(float(result)/1000)) )
 	f.close()
+	unlock(flock)
 	return
+
+def lock(fname):
+	open(fname, 'a').close()
+
+def unlock(fname):
+	if os.path.isfile(fname):
+		os.remove(fname)
 
 if __name__ == "__main__":
 	daemon = MyDaemon('/tmp/raspdiagd-11.pid')
