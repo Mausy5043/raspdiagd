acd7a3e770caf08c74880ebe711cd77befa351f3
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-05-10 13:45:07 +0200

    added `install.sh` with to-do list

---
+++ install.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# `raspbian-ua-netinst` installs `raspboot` and makes sure it is run at the
+# very first boot.
+
+# `raspboot` then installs additional packages and modifies the system
+# configuration accordingly. Among others `raspdiagd` may be installed
+# using `git clone`. Followed by calling this `install.sh` script
+
+# Installing `raspboot` requires:
+# 1. Add a cronjob in `/etc/cron.d/` periodically running `00-scriptmanager`
+#    to keep the daemons up-to-date
+# 2. Add various start-stop scripts to `/etc/init.d/` to start the daemons
+#    automagically at re-boot.
+# 3. start each of the daemons for the first time.
+
+exit 0
