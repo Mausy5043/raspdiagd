bd933874a876abb283f6bd15d626524ebd2aad80
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-05-16 16:21:41 +0200

    20150516.1621

--- testsoup.py
+++ testsoup.py
@@ -6,22 +6,22 @@ from bs4 import BeautifulSoup
 
 s1  = time.time()
 req = Request("http://xml.buienradar.nl/")
-print time.time() - s1
+print "Request         = {0}".format(time.time() - s1)
 s2 = time.time()
 response = urlopen(req)
-print time.time() - s2
+print "URLopen         = {0}".format(time.time() - s2)
 s3 = time.time()
 output = response.read()
-print time.time() - s3
+print "Read            = {0}".format(time.time() - s3)
 s4 = time.time()
 soup = BeautifulSoup(output)
-print time.time() - s4
+print "Soup            = {0}".format(time.time() - s4)
 s5 = time.time()
 
 MSwind = str(soup.buienradarnl.weergegevens.actueel_weer.weerstations.find(id=6350).windsnelheidms)
 GRwind = str(soup.buienradarnl.weergegevens.actueel_weer.weerstations.find(id=6350).windrichtinggr)
 ms = MSwind.replace("<"," ").replace(">"," ").split()[1]
 gr = GRwind.replace("<"," ").replace(">"," ").split()[1]
-print time.time() - s5
+print "Extracting info = {0}".format(time.time() - s5)
 
 print 'speed: {0} m/s, direction: {1} deg, processing time: {2} s'.format(ms, gr, time.time() - s1)
