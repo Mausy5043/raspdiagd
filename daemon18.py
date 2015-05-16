7519c8c38ceb86cb641b85eb466c90141ae23861
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-05-16 09:12:23 +0200

    add debug code

--- daemon18.py
+++ daemon18.py
@@ -56,6 +56,7 @@ class MyDaemon(Daemon):
 
 			waitTime = sampleTime - (time.time() - startTime) - (startTime % sampleTime)
 			if (waitTime > 0):
+				print "Waiting {0} s".format(waitTime)
 				time.sleep(waitTime)
 
 def gettelegram(cmd):
