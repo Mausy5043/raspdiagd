da494facd037116c6abddda8bd734d416df357e4
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-05-28 12:24:27 +0200

    extra output generated during install

--- install.sh
+++ install.sh
@@ -4,16 +4,17 @@
 # very first boot.
 
 # `raspboot` then installs additional packages and modifies the system
-# configuration accordingly. Among others `raspdiagd` may be installed
+# configuration accordingly. Among others, `raspdiagd` may be installed
 # using `git clone`. Followed by calling this `install.sh` script
 
-# Installing `raspboot` requires:
+# Installing `raspdiagd` requires:
 # 1. Add a cronjob in `/etc/cron.d/` periodically running `00-scriptmanager`
 #    to keep the daemons up-to-date
 # 2. Add various start-stop scripts to `/etc/init.d/` to start the daemons
-#    automagically at re-boot.
-# 3. start each of the daemons for the first time.
+#    automagically at re-boot. (currently using a cronjob)
 
+echo -n "Started installing raspdiagd on "
+date
 # To suppress git detecting changes by chmod:
 git config core.fileMode false
 # set the branch
@@ -29,4 +30,7 @@ if [ ! -e /mnt/share1 ]; then
   sudo mkdir /mnt/share1
 fi
 
-exit 0
+./00-scriptmanager.sh
+
+echo -n "Finished installation of raspdiagd on "
+date
