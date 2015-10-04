42b79e09f1c4a5901682b6f08f9306cd79c9f6db
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-10-04 09:57:49 +0200

    add syslog_trace function

--- dtest.py
+++ dtest.py
@@ -18,6 +18,13 @@ def wrapper(func, *args, **kwargs):
         return func(*args, **kwargs)
     return wrapped
 
+def syslog_trace(trace):
+	'''Log a python stack trace to syslog'''
+	log_lines = trace.split('\n')
+	for line in log_lines:
+		if len(line):
+			syslog.syslog(syslog.LOG_ALERT,line)
+
 if __name__ == '__main__':
   try:
     ux = getuxtime(1)
