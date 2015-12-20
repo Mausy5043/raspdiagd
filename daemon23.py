df9dc9ddae305a29093afcb970a52c0862541f65
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-12-20 10:48:44 +0100

    swap cycles and samples/cycle

--- daemon23.py
+++ daemon23.py
@@ -23,8 +23,8 @@ IS_SYSTEMD = os.path.isfile('/bin/journalctl')
 class MyDaemon(Daemon):
   def run(self):
     reportTime = 60                                 # time [s] between reports
-    cycles = 3                                      # number of cycles to aggregate
-    samplesperCycle = 5                             # total number of samples in each cycle
+    cycles = 5                                      # number of cycles to aggregate
+    samplesperCycle = 3                             # total number of samples in each cycle
     samples = samplesperCycle * cycles              # total number of samples averaged
     sampleTime = reportTime/samplesperCycle         # time [s] between samples
     cycleTime = samples * sampleTime                # time [s] per cycle
